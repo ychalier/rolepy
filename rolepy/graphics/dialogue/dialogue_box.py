ac05ad3e6d374caa7b24276641b3ada1088e27a8
gtallec <gauthier.tallec996@gmail.com>
2019-10-27 17:55:17 +0100

    send code to server

---
+++ rolepy/graphics/dialogue/dialogue_box.py
@@ -0,0 +1,13 @@
+
+
+class DialogueManager:
+    """Manage dialogue box, those handle message from the game and dialogue between players."""
+    def __init__(self, resolution):
+        self.resolution = resolution
+        self
+
+        
+        
+
+        
+    def blit(self,
